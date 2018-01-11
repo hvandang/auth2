34ad257d2ac1f0d7ae0506d305a0b89e58bb0e27
Hai-Van <hvandang@outlook.com>
2018-01-11 12:58:22 +0000

    chang host

--- my_script.py
+++ my_script.py
@@ -30,4 +30,4 @@ def check_auth(username, password):
 api.add_resource(Authenticate, '/auth')
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5002)
+    app.run(host= '0.0.0.0',port=5002)
